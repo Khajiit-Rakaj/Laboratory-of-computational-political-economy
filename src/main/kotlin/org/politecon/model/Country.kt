590c541117edd9c0a3b53256cebf4ef21d2ab248
Danil Kornishev <danil.kornishev@gmail.com>
2023-01-03 16:47:25 -0500

    Added basic country enum search Added basic test for country enum search

--- src/main/kotlin/org/politecon/model/Country.kt
+++ src/main/kotlin/org/politecon/model/Country.kt
@@ -1,5 +1,12 @@
 package org.politecon.model
 
-enum class Country(twoLetterCode: String, threeLetterCode: String) {
-    RUSSIA("RU", "RUS"), USA("US", "USA")
+enum class Country(val twoLetterCode: String, val threeLetterCode: String) {
+    RUSSIA("RU", "RUS"), USA("US", "USA"),
+    UNKNOWN("XX", "XXX")
+    ;
+
+    companion object {
+        fun fromString(input: String): Country =
+            values().find { input.uppercase() in listOf(it.twoLetterCode, it.threeLetterCode, it.name) } ?: UNKNOWN
+    }
 }
