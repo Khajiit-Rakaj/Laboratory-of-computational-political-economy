464d90c16c2c8190edb0bd6117868801bec286b5
Danil Kornishev <danil.kornishev@gmail.com>
2023-02-22 13:35:52 -0500

    (Фича): Начало АПИ модуля (Фича): Начало YAML properties

--- modules/api/build.gradle.kts
+++ modules/api/build.gradle.kts
@@ -1,14 +1,43 @@
 plugins {
     kotlin("jvm")
+    kotlin("plugin.serialization")
+    id("io.ktor.plugin") version "2.2.3"
+    application
 }
 
-group = "org.politecon"
-version = "unspecified"
+group = "org.politecon.api"
+version = "0.0.1"
+application {
+    mainClass.set("org.politecon.api.ApplicationKt")
+
+    val isDevelopment: Boolean = true //project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
 
 repositories {
     mavenCentral()
 }
 
+val ktorVersion = "2.2.3"
+val kotlinVersion = "1.8.10"
+val jacksonVersion = "2.14.1"
+
 dependencies {
-    implementation(kotlin("stdlib"))
+    implementation(project(":modules:common"))
+    implementation(project(":modules:storage"))
+
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-host-common-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-compression-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+
+    // Сериализация
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
 }
