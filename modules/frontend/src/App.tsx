a1b2f10fef3a89a80de2a88a5ff3f50547554195
Vitaly <72764133+vitlyc@users.noreply.github.com>
2023-03-01 22:33:22 +0300

    add routes

--- modules/frontend/src/App.tsx
+++ modules/frontend/src/App.tsx
@@ -1,20 +1,28 @@
 import styles from "./App.module.css"
+import { Routes, Route, Navigate } from "react-router-dom"
 import DATA from "./corp_finance.json"
 import Header from "./components/Header/Header"
 import Table from "./components/Table/Table"
+import About from "./components/About/About"
+import Login from "./components/Login/Login"
 
 export const App = () => {
   console.log(DATA)
 
   return (
-    <>
+    <div className={styles.app}>
       <Header />
-      <div className={styles.app}>
-        <main className={styles.content}>
-          <h1>Hello</h1>
-        </main>
-        <Table />
-      </div>
-    </>
+      <Routes>
+        <Route path="/" element={<Table />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   )
 }
+//  ;<div className={styles.app}>
+//    <main className={styles.content}>
+//      <h1>Hello</h1>
+//    </main>
+//    <Table />
+//  </div>
