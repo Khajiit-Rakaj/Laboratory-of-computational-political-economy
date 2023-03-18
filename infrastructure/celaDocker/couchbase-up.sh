9a24ff29c92a8976024708acc45717a10cd4d9cf
ikeksh <ikeksh@gmail.com>
2023-03-18 03:19:50 +0300

    Добавил создание primary index для коллекций (#16)

--- infrastructure/celaDocker/couchbase-up.sh
+++ infrastructure/celaDocker/couchbase-up.sh
@@ -28,6 +28,14 @@ curl -v -u Admin:rootPass -X POST http://127.0.0.1:8091/pools/default/buckets/po
 curl -v -u Admin:rootPass -X POST http://127.0.0.1:8091/pools/default/buckets/politecon/scopes/_default/collections -d name=economics
 curl -v -u Admin:rootPass -X POST http://127.0.0.1:8091/pools/default/buckets/politecon/scopes/_default/collections -d name=patents
 
+echo "\n\nCreating collections primary indices\n"
+
+curl -v -u Admin:rootPass -X POST http://127.0.0.1:8093/query/service?statement=create%20primary%20index%20on%20default:politecon._default.commodity_data
+curl -v -u Admin:rootPass -X POST http://127.0.0.1:8093/query/service?statement=create%20primary%20index%20on%20default:politecon._default.population_data
+curl -v -u Admin:rootPass -X POST http://127.0.0.1:8093/query/service?statement=create%20primary%20index%20on%20default:politecon._default.corporate_finance
+curl -v -u Admin:rootPass -X POST http://127.0.0.1:8093/query/service?statement=create%20primary%20index%20on%20default:politecon._default.economics
+curl -v -u Admin:rootPass -X POST http://127.0.0.1:8093/query/service?statement=create%20primary%20index%20on%20default:politecon._default.patents
+
 curl -v -u Admin:rootPass -X GET http://127.0.0.1:8091/pools/default/buckets/politecon/scopes/
 
 echo "\n\nCreate local user\n"
